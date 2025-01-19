647e2cb8e34e6c28530b655157b4e398c3ac1d73
Mohamed Hossam <plcacademy123@gmail.com>
2025-01-19 13:07:51 +0200

    Update next.config.ts

--- game-bara-elsalfa/next.config.ts
+++ game-bara-elsalfa/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
